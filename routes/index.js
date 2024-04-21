52faab50a7551929d28f31cac80e54eb426e1b51
Rikko <bulletbill51@yahoo.com>
2024-04-20 19:53:21 -0700

    Created form for new route to render
    Form posts request to index
    Index pushes request to messages array
    Added button to direct to /new page on index

--- routes/index.js
+++ routes/index.js
@@ -20,4 +20,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Mini Message Board', messages: messages });
 });
 
+router.post('/new', function(req, res, next) {
+  const messageText = req.body.message;
+  const messageUser = req.body.user;
+  messages.push( {text: messageText, user: messageUser, added: new Date()});
+  res.redirect('/')
+})
+
+router.post('/redirect', function(req, res, next) {
+  res.redirect('/new')
+})
+
 module.exports = router;
